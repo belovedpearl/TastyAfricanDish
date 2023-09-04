3cbee2bde71fcb81544240d3646bb9d7b35573af
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-09-04 09:16:57 +0000

    add style to the index and details page

--- recipeblog/urls.py
+++ recipeblog/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.RecipeView.as_view(), name='home'),
+    path('details/<slug:slug>', views.RecipeDetails.as_view(), name='recipe-detail'),
+
 ]
--- recipeblog/views.py
+++ recipeblog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views import generic
 from .models import Recipe
 
 
-class RecipeView(ListView):
+class RecipeView(generic.ListView):
     model = Recipe
     queryset = Recipe.objects.filter(post_approved=True).order_by('-date_created')
     template_name = "index.html"
     paginate_by = 8
+
+class RecipeDetails(generic.DetailView,):
+    model = Recipe
+    template_name = "details.html"
